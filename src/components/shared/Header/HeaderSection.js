dfb4a6768c94c2b35533fd1bc782d580988bae26
felix kithinji <felixkithinji@outlook.com>
2021-07-02 12:06:24 +0300

    add version

--- src/components/shared/Header/HeaderSection.js
+++ src/components/shared/Header/HeaderSection.js
@@ -6,7 +6,7 @@ export const HeaderSection = ({ facility }) => (
   <Container className="header-section">
     <Header as="h1" className="header-text">
       <img src={logo} className="App-logo" alt="logo" />
-      Interoperability Layer{" "}
+      Interoperability Layer{" - ver 4.0.0 "}
       {facility ? (
         <span className="facility-name">({facility}) </span>
       ) : (
